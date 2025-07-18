7f24bb869f7b0783be47f54a7835452e12a045d4
Oleh <zaaqq135@gmail.com>
2025-07-19 00:27:55 +0300

    updated routes using elysia

--- index.ts
+++ index.ts
@@ -3,26 +3,28 @@ import Elysia from "elysia";
 
 const port = 3000;
 const server = new Elysia()
-    .onError(({ code, error }) => {
-        return new Response(`Error <b>${code}</b>: <span style="color: red">${error.toString()}</span>`,
-            {
-                status: 500,
-                headers: { 'Content-Type': 'text/html' }
-            });
+    .onError(({ code, error, set }) => {
+        if (code === 'NOT_FOUND') {
+            return figlet.textSync("404");
+        }
+
+        set.headers = { 'content-type': 'text/html' };
+        set.status = 500;
+        return `Error <b>${code}</b>: <span style="color: red">${error.toString()}</span>`;
     })
-    .get("/", _ => new Response(figlet.textSync("Hello world!")))
-    .get("/about", _ => new Response(figlet.textSync("About me!")))
+    .get("/", _ => figlet.textSync("Hello world!"))
+    .get("/about", _ => figlet.textSync("About me!"))
     .get("/feed", (_): never => {
         throw new Error("Abandoned page!");
     })
-    .get("/greet", async (_): Promise<Response> => {
+    .get("/greet", async ({ set }): Promise<string> => {
         const text = await Bun.file('storage/files/greet.txt').text();
-        return new Response(`<pre>${text}</pre>`, {
-            headers: {
-                'Content-Type': 'text/html'
-            }
-        });
+
+        set.headers = { 'content-type': 'text/html' };
+        return `<pre>${text}</pre>`;
+
     })
+    .get("/post/:id", ({ params: { id } }) => figlet.textSync(`Post ${id}`))
     .listen(port);
 
 console.log(`Listening on PORT http://localhost:${server.server?.port}`);
