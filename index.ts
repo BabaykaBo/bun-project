2694cc3491fe50f0c2e423bc3600632f0da36d80
Oleh <zaaqq135@gmail.com>
2025-07-18 23:17:37 +0300

    primitive routes

--- index.ts
+++ index.ts
@@ -1,1 +1,25 @@
-console.log("Hello via Bun!");
+import figlet, { fonts } from "figlet";
+
+const server = Bun.serve({
+    port: 3000,
+    fetch(req) {
+        const url = new URL(req.url);
+
+        let text: string;
+        switch (url.pathname) {
+            case '/':
+                text = "Hello world!";
+                break;
+            case '/about':
+                text = "About me!";
+                break;
+            default:
+                text = "404";
+                break;
+        }
+
+        return new Response(figlet.textSync(text));
+    }
+})
+
+console.log(`Listening on PORT http://localhost:${server.port}`);
